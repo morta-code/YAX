605149d2f15aa6998319718a2fa7738f65a5d190
morta-code <morta@digitus.itk.ppke.hu>
2015-05-14 09:20:26 +0200

    Using built-in xml instead of lxml (bc. of a bug).

--- yax/YAXReader.py
+++ yax/YAXReader.py
@@ -5,12 +5,11 @@ import re
 
 RE = type(re.compile(""))
 try:
+    raise ImportError("LXML module has a bug.")
     import lxml.etree as etree
-
     LXML = True
 except ImportError:
     import xml.etree.ElementTree as etree
-
     LXML = False
 
 
@@ -89,7 +88,8 @@ class Condition():
         elif isinstance(tag, str):  # The lam.expr. will compare
             return lambda s: s == tag
         elif isinstance(tag, RE):  # The l.exp. checks with fullmatch
-            return lambda s: s is not None and tag.fullmatch(s) is not None
+            # return lambda s: s is not None and tag.fullmatch(s) is not None
+            return lambda s: tag.fullmatch(s) is not None
         elif isinstance(tag, list):  # The l.ex. checks the containing (str!)
             taglist = list()
             for t in tag:
@@ -137,7 +137,12 @@ class Condition():
                  parent=None, children=None, keep_children=None):
 
         self._inverted = False  # self doesn't matches if would be match
-        self.check = self._check  # set the non-inverted check function
+
+        # The LXML way has different methods:
+        self.check = self._check_lxml if LXML else self._check_xml
+        self.keep = self._keep_lxml if LXML else self._keep_xml
+        self._check_children = self._check_children_lxml if LXML else self._check_children_xml
+
         # condition attributes (check callables will be created):
         self._tag = Condition.normalize_tag(tag)
         self._attrib = Condition.normalize_attrib(attrib)
@@ -153,12 +158,12 @@ class Condition():
         """
         self._inverted = not self._inverted
         if self._inverted:
-            self.check = self._inverted_check
+            self.check = self._inverted_check_lxml if LXML else self._inverted_check_xml
         else:
-            self.check = self._check
+            self.check = self._check_lxml if LXML else self._check_xml
         return self
 
-    def _check_children(self, element):
+    def _check_children_lxml(self, element):
         children = list(element)  # Every child-condition must be matching to a
         for ch_cond in self._children:  # child
             found = False
@@ -170,38 +175,67 @@ class Condition():
                 return False
         return True
 
-    def _check(self, element) -> bool:
+    def _check_children_xml(self, element):
+        children = list(element)  # Every child-condition must be matching to a
+        for ch_cond in self._children:  # child
+            found = False
+            for child in children:
+                if ch_cond.check(child, []):  # There is no way to check children's parents!
+                    found = True
+                    break
+            if not found:
+                return False
+        return True
+
+    def _check_lxml(self, element) -> bool:
         try:
             # If any part of condition is false, return with false.
             if not self._tag(element.tag):                          # Checking tagname
                 return False
             if not self._attrib(element.attrib):                    # Checking attribs
                 return False
-            # if not self._text(element.text):                  # Checking text
-            #     return False
-            #  todo: Üres text vs. whitespace
-            if element.text is None:
+            if element.text is None:                                # Checking text
                 if not self._text(None):
                     return False
             else:
                 if not self._text(element.text.strip()):
                     return False
-
-            # CSAK lxml.ElementTree todo: WORKAROUND
-            if LXML:
-                if not self._parent.check(element.getparent()):     # Checking parent
-                    return False
-
+            if not self._parent.check(element.getparent()):     # Checking parent
+                return False
             if not self._check_children(element):                   # Checking children
                 return False
         except:
             return False
         return True
 
-    def _inverted_check(self, element) -> bool:
-        return not self._check(element)
+    def _check_xml(self, element, parents) -> bool:
+        try:
+            # If any part of condition is false, return with false.
+            if not self._tag(element.tag):                          # Checking tagname
+                return False
+            if not self._attrib(element.attrib):                    # Checking attribs
+                return False
+            if element.text is None:                                # Checking text
+                if not self._text(None):
+                    return False
+            else:
+                if not self._text(element.text.strip()):
+                    return False
+            if not self._parent.check(parents[-1] if len(parents) > 0 else None, parents[:-1]):
+                    return False
+            if not self._check_children(element):                   # Checking children
+                return False
+        except:
+            return False
+        return True
 
-    def keep(self, element) -> bool:
+    def _inverted_check_lxml(self, element) -> bool:
+        return not self._check_lxml(element)
+
+    def _inverted_check_xml(self, element) -> bool:
+        return not self._check_xml(element)
+
+    def _keep_lxml(self, element) -> bool:
         parent = element.getparent()
         if parent is None:
             return True
@@ -212,6 +246,19 @@ class Condition():
                 return True
         for keep_cond in self._keep:                    # Keep if it is in the keep conditions
             if keep_cond.check(element):
+                return True
+        return False
+
+    def _keep_xml(self, element, parents) -> bool:
+        if len(parents) == 0:
+            return True
+        if not self._tag(parents[-1].tag):     # Element's parent must be match
+            return False
+        for ch_cond in self._children:                  # Keep if it is in the children conditions
+            if ch_cond.check(element, parents):
+                return True
+        for keep_cond in self._keep:                    # Keep if it is in the keep conditions
+            if keep_cond.check(element, parents):
                 return True
         return False
 
@@ -266,6 +313,7 @@ class CallbackRunner():
                 d[CallbackRunner.ATTRIB_PREFIX + a] = v
 
         for child in list(e):
+            text.append(child.tail.strip() if child.tail is not None else "")
             ch = CallbackRunner._convert_to_json_dict(child)
             if d.get(child.tag):
                 c = d[child.tag]
@@ -276,12 +324,13 @@ class CallbackRunner():
             else:
                 d[child.tag] = ch[child.tag]
 
-            text.append(child.tail.strip() if child.tail is not None else "")
 
         # clean text
+        t2 = []
         for t in text:
-            if t == "":
-                text.remove(t)
+            if t:
+                t2.append(t)
+        text = t2
         if len(text) == 1:
             text = text[0]
         # add text if exists
@@ -339,35 +388,48 @@ class YAXReader():
             self._stream.close()
         del self._stream
 
-    def __del__(self):
-        del self.stream
-
-    def start(self, chunk_size=10000):
-        # This is the main part of the element processing.
-        def process_element(e):
-            keep = False                            # Do not keep anything by default.
-            for cond, cb_runner in self._cnds:      # For all conditions.
-                if cond.check(e):                   # When matches, run the callback.
-                    cb_runner(e)
-                if keep:                            # If already keep, go to next.
-                    continue
-                if cond.keep(e):                    # If has to be kept, set keep.
-                    keep = True
-            if not keep:                            # After all condition delete if not keep.
-                del e.getparent()[e.getparent().index(e)]
-
+    def start(self, chunk_size=8192):
         if not self._stream:
             raise AttributeError("Input stream is not initialized.")
         elif self._stream.closed:
             raise AttributeError("The input stream is closed.")
-        parser = etree.XMLPullParser(events=('end',))
-        # Reading the stream until end.
-        chunk = self._stream.read(chunk_size)
-        while not chunk == "":
-            parser.feed(chunk)
-            for action, element in parser.read_events():
-                process_element(element)
+        if LXML:
+            parser = etree.XMLPullParser(events=('end',))
             chunk = self._stream.read(chunk_size)
+            while chunk:
+                parser.feed(chunk)
+                for action, element in parser.read_events():
+                    keep = False                            # Do not keep anything by default.
+                    for cond, cb_runner in self._cnds:      # For all conditions.
+                        if cond.check(element):             # When matches, run the callback.
+                            cb_runner(element)
+                        if not keep and cond.keep(element):         # If has to be kept, set keep.
+                            keep = True
+                    if not keep:                        # After all condition delete if not keep.
+                        # del element.getparent()[element.getparent().index(element)]
+                        element.getparent().remove(element)
+                chunk = self._stream.read(chunk_size)
+        else:
+            parser = etree.XMLPullParser(events=('end', 'start'))
+            parents = []
+            chunk = self._stream.read(chunk_size)
+            while chunk:
+                parser.feed(chunk)
+                for action, element in parser.read_events():
+                    if action == 'start':
+                        parents.append(element)
+                    else:
+                        parents.pop()
+                        keep = False                            # Do not keep anything by default.
+                        for cond, cb_runner in self._cnds:      # For all conditions.
+                            if cond.check(element, parents):
+                                cb_runner(element)
+                            if not keep and cond.keep(element, parents):
+                                keep = True
+                        if not keep:                        # After all condition delete if not keep.
+                            parents[-1].remove(element)
+                chunk = self._stream.read(chunk_size)
+        del self.stream
 
     def find_as_element(self, tag=None, attrib={}, text=None,
                         parent=None, children=None, keep_children=None) -> CallbackRunner:
